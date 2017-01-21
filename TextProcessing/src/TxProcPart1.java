b84ecc8eaa4f2813bf153d34ec7c62ce5458f4ee
cjk987213@gmail.com <GhmM09ByW>
2017-01-20 22:08:00 -0800

    Try again, lol

---
+++ TextProcessing/src/TxProcPart1.java
@@ -0,0 +1,10 @@
+/**
+ * Created by cjk98 on 1/20/2017.
+ * For CMPSCI_221
+ */
+public class TxProcPart1 {
+    public static void main (String arg[]){
+        System.out.println("TxProcPart1.main");
+        System.out.println(arg[0]);
+    }
+}

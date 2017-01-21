97be6326297d002af9877f46298cd22d205f593d
cjk987213@gmail.com <GhmM09ByW>
2017-01-21 09:34:13 -0800

    Just start, stated APIs

--- TextProcessing/src/TxProcPart1.java
+++ TextProcessing/src/TxProcPart1.java
@@ -1,8 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by cjk98 on 1/20/2017.
  * For CMPSCI_221
  */
 public class TxProcPart1 {
+
+    public List<String> tokenize(String textFilePath) {
+        return new ArrayList<>();
+    }
+
+    public Map<String, Integer> computeWordFrequencies(List<String> tokenList) {
+        return new HashMap<>();
+    }
+
+    public void print(HashMap<String, Integer> frequenciesMap) {
+
+    }
+
     public static void main (String arg[]){
         System.out.println("TxProcPart1.main");
         System.out.println(arg[0]);

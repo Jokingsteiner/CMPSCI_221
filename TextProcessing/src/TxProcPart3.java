1309d9cc16607eafd35a84878da5b2ba4d46b1a4
cjk987213@gmail.com <GhmM09ByW>
2017-01-21 22:46:13 -0800

    Initial test passed

--- TextProcessing/src/TxProcPart3.java
+++ TextProcessing/src/TxProcPart3.java
@@ -1,19 +1,18 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by cjk98 on 1/21/2017.
  * to reduce word map (merge 2 word maps into 1)
  */
 public class TxProcPart3 {
+//    private static final Comparator<Map.Entry<String, Integer>> BY_KEY_ALPHABETICALLY = new ByKeyAlphabetically();
     private FileReaderWBuffer fr1, fr2;
     private FileWriterWBuffer fw;
-    private HashMap<String, Integer> finalMap;
+    // ordered with natural order of keys
+    private TreeMap<String, Integer> finalMap;
 
     public TxProcPart3(String filePath1, String filePath2, String destPath) throws FileNotFoundException {
         fr1 = new FileReaderWBuffer(filePath1);
@@ -22,19 +21,20 @@ public class TxProcPart3 {
         if (file.exists())
             file.delete();
         fw = new FileWriterWBuffer(destPath, true);
-        this.finalMap = new HashMap<>();
+        this.finalMap = new TreeMap<>();
     }
 
     public void merge(){
         String line;
         while ((line = fr1.readLine()) != null){
             List<String> tokens = getTokenFromString(line);
-            finalMap.put(tokens.get(0), Integer.valueOf(finalMap.getOrDefault(tokens.get(0), 0) + tokens.get(1)));
+            finalMap.put(tokens.get(0), finalMap.getOrDefault(tokens.get(0), 0) + Integer.valueOf(tokens.get(1)));
         }
         while ((line = fr2.readLine()) != null){
             List<String> tokens = getTokenFromString(line);
-            finalMap.put(tokens.get(0), Integer.valueOf(finalMap.getOrDefault(tokens.get(0), 0) + tokens.get(1)));
+            finalMap.put(tokens.get(0), finalMap.getOrDefault(tokens.get(0), 0) + Integer.valueOf(tokens.get(1)));
         }
+
         for (Map.Entry<String, Integer> s : finalMap.entrySet()){
             fw.writeLine(s.getKey() + ", " + s.getValue());
         }
@@ -42,6 +42,13 @@ public class TxProcPart3 {
         fr2.close();
         fw.close();
     }
+/*
+    private static class ByKeyAlphabetically implements Comparator<Map.Entry<String, Integer>> {
+        @Override
+        public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+            return o1.getKey().compareTo(o2.getKey());
+        }
+    }*/
 
     private List<String> getTokenFromString (String str) {
         List<String> tokenList = new ArrayList<>();
